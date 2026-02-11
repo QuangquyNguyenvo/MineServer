fc6292d307e3c9a11a95887acb1f4e56544f78e4
QuangquyNguyenvo <tytokg0002@gmail.com>
2026-02-11 13:26:17 +0700

    tối ưu

--- start.bat
+++ start.bat
@@ -48,7 +48,27 @@ echo.
 
 powershell -Command "(Get-Content 'server.properties') -replace 'server-port=\d+', 'server-port=%SERVER_PORT%' | Set-Content 'server.properties'"
 
-java -Xms2G -Xmx2G -Dfile.encoding=UTF-8 -jar paper-1.21.11-100.jar nogui
+java -Xms4G -Xmx4G ^
+    -XX:+UseG1GC ^
+    -XX:+ParallelRefProcEnabled ^
+    -XX:MaxGCPauseMillis=200 ^
+    -XX:+UnlockExperimentalVMOptions ^
+    -XX:+DisableExplicitGC ^
+    -XX:+AlwaysPreTouch ^
+    -XX:G1NewSizePercent=30 ^
+    -XX:G1MaxNewSizePercent=40 ^
+    -XX:G1HeapRegionSize=8M ^
+    -XX:G1ReservePercent=20 ^
+    -XX:G1HeapWastePercent=5 ^
+    -XX:G1MixedGCCountTarget=4 ^
+    -XX:InitiatingHeapOccupancyPercent=15 ^
+    -XX:G1MixedGCLiveThresholdPercent=90 ^
+    -XX:G1RSetUpdatingPauseTimePercent=5 ^
+    -XX:SurvivorRatio=32 ^
+    -XX:+PerfDisableSharedMem ^
+    -XX:MaxTenuringThreshold=1 ^
+    -Dfile.encoding=UTF-8 ^
+    -jar paper-1.21.11-100.jar nogui
 
 echo.
 echo Server da dung. Nhan phim bat ky de dong...
